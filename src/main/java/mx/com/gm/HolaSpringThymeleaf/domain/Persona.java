3c55aedaaa23a1a52511bb3ba991b7b276aa521e
Florencia <florencia.castro@bibamgroup.com>
2020-06-14 02:11:54 -0300

    Cree una persona

---
+++ src/main/java/mx/com/gm/HolaSpringThymeleaf/domain/Persona.java
@@ -0,0 +1,11 @@
+package mx.com.gm.HolaSpringThymeleaf.domain;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
